ed0732939701f79aa5cfc6ee3a965fef82f387d6
Jeff Fore <jfore@google.com>
2015-05-20 16:32:51 -0700

    Update device properties for latest oemcrypto engine features.
    Change-Id: Id384749f610eea39a817a411dd456bb160d91471

--- package/google/google_widevine_cenc/oemcrypto/spacecast_gfcs100_device_properties.cpp
+++ package/google/google_widevine_cenc/oemcrypto/spacecast_gfcs100_device_properties.cpp
@@ -19,18 +19,35 @@ bool CryptoEngine::closed_platform() {
 
 // Returns the HDCP version currently in use.
 OEMCrypto_HDCP_Capability CryptoEngine::current_hdcp_capability() {
-  return local_display() ? 0xFF : 0x01;
+  return static_cast<OEMCrypto_HDCP_Capability>(local_display() ? 0xFF : 0x01);
 }
 
 // Returns the max HDCP version supported.
 OEMCrypto_HDCP_Capability CryptoEngine::maximum_hdcp_capability() {
-  return 0x02;
+  return static_cast<OEMCrypto_HDCP_Capability>(0x02);
 }
 
 // Returns true if the client supports persistent storage of
 // offline usage table information.
 bool CryptoEngine::supports_storage() {
   return true;
+}
+
+// Returns true if the client uses a keybox as the root of trust.
+bool CryptoEngine::supports_keybox() {
+  return true;
+}
+
+// Returns false for mock library to indicate the client does not support
+// anti-rollback hardware.
+bool CryptoEngine::is_anti_rollback_hw_present() {
+  return false;
+}
+
+// Returns "L3" for a software only library.  L1 is for hardware protected
+// data paths.
+const char* CryptoEngine::security_level() {
+  return "L3";
 }
 
 }  // namespace wvoec_mock
