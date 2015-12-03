lw-davidt/gfiber-buildroot

commit e6f7488fa7297f88b12ee0169e50d067331e4219

gfch100: move platform specifics into google_platform Makefiles
Change-Id: Id455df7a4f30f29912b9770e5f23a2097f67a683

package/google/google_platform/google_platform.mk
@@ -109,19 +109,6 @@ endif
 
 PUB_KEY=gfiber
 
-ifeq ($(BR2_PACKAGE_GOOGLE_STORAGE_BOX),y)
-BR2_TARGET_GOOGLE_PLATFORM = gfibertv
-else ifeq ($(BR2_PACKAGE_GOOGLE_FIBER_JACK),y)
-BR2_TARGET_GOOGLE_PLATFORM = gfiberlt
-else ifeq ($(BR2_PACKAGE_GOOGLE_NETWORK_BOX),y)
-BR2_TARGET_GOOGLE_PLATFORM = gfibertv
-else ifeq ($(BR2_TARGET_GENERIC_PLATFORM_NAME),"gfsc100")
-BR2_TARGET_GOOGLE_PLATFORM = gfibersc
-PUB_KEY=gfibersc
-else ifeq ($(BR2_PACKAGE_GOOGLE_WINDCHARGER),y)
-BR2_TARGET_GOOGLE_PLATFORM = gfiberwc
-endif
-
 ifneq ($(BR2_PACKAGE_GOOGLE_KEY_SUFFIX),"")
 PUB_KEY := $(PUB_KEY)-$(patsubst "%",%,$(BR2_PACKAGE_GOOGLE_KEY_SUFFIX))
 endif
@@ -142,7 +129,6 @@ GPLAT_BASEMAKE = \
 	BR2_PACKAGE_BCM_NEXUS=$(BR2_PACKAGE_BCM_NEXUS) \
 	BR2_PACKAGE_MINDSPEED_DRIVERS=$(BR2_PACKAGE_MINDSPEED_DRIVERS) \
 	BR2_TARGET_GENERIC_PLATFORM_NAME=$(BR2_TARGET_GENERIC_PLATFORM_NAME) \
-	BR2_TARGET_GOOGLE_PLATFORM=$(BR2_TARGET_GOOGLE_PLATFORM) \
 	BUILD_HNVRAM=$(BR2_PACKAGE_GOOGLE_PLATFORM_HNVRAM) \
 	BUILD_SSDP=$(BR2_PACKAGE_MINIUPNPD) \
 	BUILD_DNSSD=$(BUILD_DNSSD) \

commit 7c0cb8085eca8220bbcffd73dca2a5645a452e52

google_platform: Fix support for platform specific pubkeys
Commit e6f7488fa ("gfch100: move platform specifics into google_platform
Makefiles") broke support for platform specific public keys, making all
platforms use the default Fiber key.

Change-Id: Iab4c37118592b0c7499ab6207d9cf3145ccd4c18

package/google/google_platform/google_platform.mk
@@ -109,6 +109,10 @@ endif
 
 PUB_KEY=gfiber
 
+ifeq ($(BR2_TARGET_GENERIC_PLATFORM_NAME),"gfsc100")
+PUB_KEY=gfibersc
+endif
+
 ifneq ($(BR2_PACKAGE_GOOGLE_KEY_SUFFIX),"")
 PUB_KEY := $(PUB_KEY)-$(patsubst "%",%,$(BR2_PACKAGE_GOOGLE_KEY_SUFFIX))
 endif