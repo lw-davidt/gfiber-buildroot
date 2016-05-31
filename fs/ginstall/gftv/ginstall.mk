lw-davidt/gfiber-buildroot

commit e0a13cc92aaa4107c7b8869a548fd8d328cebd92

Add support for xz compression of simpleramfs
Google-Bug-Id: 24579073
Google-Bug-Id: 26519506

Change-Id: Ic6b62b84d392a2d7bcb2b760494a5b78b1f5fe16

fs/ginstall/gftv/ginstall.mk
@@ -15,6 +15,13 @@ endif
 
 ifeq ($(BR2_PACKAGE_SIMPLERAMFS),y)
 ROOTFS_GINSTALL_DEPENDENCIES += simpleramfs
+ifeq ($(BR2_PACKAGE_SIMPLERAMFS_XZ),y)
+INITRAMFS_IMAGE=initramfs.cpio.xz
+INITRAMFS_COMPRESS_COMMAND='xz -c --check=crc32 --lzma2=dict=1MiB'
+else
+INITRAMFS_IMAGE=initramfs.cpio.gz
+INITRAMFS_COMPRESS_COMMAND='gzip -c'
+endif
 endif
 
 ifeq ($(BR2_TARGET_ROOTFS_INITRAMFS),y)
@@ -156,7 +163,7 @@ OPTIMUS_SIGNING=y
 BUILD_UIMAGE=y
 MKIMAGE_KERNEL_LOAD_ADDRESS = 0x04008000
 MKIMAGE_KERNEL_ENTRY_POINT = 0x04008000
-MKIMAGE_DATA_FILE=zImage:initramfs.cpio.gz
+MKIMAGE_DATA_FILE=zImage:$(INITRAMFS_IMAGE)
 MKIMAGE_IMAGE_TYPE=multi
 MKIMAGE_COMPRESSION_TYPE=none
 MKIMAGE_EXTRA_FLAGS=
@@ -211,7 +218,7 @@ OPTIMUS_SIGNING=y
 BUILD_UIMAGE=y
 MKIMAGE_KERNEL_LOAD_ADDRESS = 0x04008000
 MKIMAGE_KERNEL_ENTRY_POINT = 0x04008000
-MKIMAGE_DATA_FILE=zImage:initramfs.cpio.gz:gfch100.dtb
+MKIMAGE_DATA_FILE=zImage:$(INITRAMFS_IMAGE):gfch100.dtb
 MKIMAGE_IMAGE_TYPE=multi
 MKIMAGE_COMPRESSION_TYPE=none
 MKIMAGE_EXTRA_FLAGS=
@@ -285,9 +292,9 @@ define ROOTFS_GINSTALL_CMD_V3_V4
 	if [ '$(BR2_TARGET_ROOTFS_SQUASHFS)' = 'y' ]; then \
 		ln -f rootfs.squashfs rootfs.img && \
 		if [ '$(BR2_TARGET_ROOTFS_RECOVERYFS)' != 'y' ]; then \
-			gzip -c <simpleramfs.cpio >initramfs.cpio.gz; \
+			$(shell echo $(INITRAMFS_COMPRESS_COMMAND)) <simpleramfs.cpio >$(INITRAMFS_IMAGE); \
 		else \
-			gzip -c <recoveryfs.cpio >initramfs.cpio.gz; \
+			$(shell echo $(INITRAMFS_COMPRESS_COMMAND)) <recoveryfs.cpio >$(INITRAMFS_IMAGE); \
 		fi; \
 	fi && \
 	if [ '$(BUILD_UIMAGE)' = 'y' ]; then \
@@ -359,7 +366,7 @@ define ROOTFS_GINSTALL_CMD_V2
 		); \
 	fi && \
 	cd $(BINARIES_DIR) && \
-	gzip -c <simpleramfs.cpio >simpleramfs.cpio.gz && \
+	$(shell echo $(INITRAMFS_COMPRESS_COMMAND)) <simpleramfs.cpio >$(INITRAMFS_IMAGE) && \
 	tar -cf $(value ROOTFS_GINSTALL_VERSION).gi \
 		version \
 		$(BRUNO_LOADERS_V2) \

package/simpleramfs/Config.in
@@ -19,3 +19,22 @@ config BR2_PACKAGE_SIMPLERAMFS_LINKED_IN
 	depends on BR2_PACKAGE_SIMPLERAMFS
 	help
 	  Turn the simpleramfs into the initramfs compiled into the kernel image.
+
+if BR2_PACKAGE_SIMPLERAMFS
+
+choice
+	prompt "Compression algorithm"
+	default BR2_PACKAGE_SIMPLERAMFS_GZIP
+	depends on !BR2_PACKAGE_SIMPLERAMFS_LINKED_IN
+	help
+	  Select the compression algorithm for the simpleramfs image.
+
+config BR2_PACKAGE_SIMPLERAMFS_GZIP
+	bool "gzip"
+
+config BR2_PACKAGE_SIMPLERAMFS_XZ
+	bool "xz"
+
+endchoice
+
+endif

commit 64b788883a3556680c2d6b42c90ef373fd365d3b

gftv/ginstall.mk: Fix gffrenzy and gfex250 builds
Commit e0a13cc ("Add support for xz compression of simpleramfs") broke
the build for gffrenzy and gfex250. The initialization of the variables
INITRAMFS_IMAGE and INITRAMFS_COMPRESS_COMMAND depends on SIMPLERAMFS,
which isn't used on these platforms. Initialize the two variables with
default values and overwrite them if applicable when SIMPLERAMFS is
enabled.

Change-Id: I2c7b30b56e651bbb2a7947e10d8f6bc05669b5c8

fs/ginstall/gftv/ginstall.mk
@@ -13,14 +13,14 @@ ifeq ($(BR2_TARGET_ROOTFS_RECOVERYFS),y)
 ROOTFS_GINSTALL_DEPENDENCIES += rootfs-recoveryfs
 endif
 
+INITRAMFS_IMAGE=initramfs.cpio.gz
+INITRAMFS_COMPRESS_COMMAND='gzip -c'
+
 ifeq ($(BR2_PACKAGE_SIMPLERAMFS),y)
 ROOTFS_GINSTALL_DEPENDENCIES += simpleramfs
 ifeq ($(BR2_PACKAGE_SIMPLERAMFS_XZ),y)
 INITRAMFS_IMAGE=initramfs.cpio.xz
 INITRAMFS_COMPRESS_COMMAND='xz -c --check=crc32 --lzma2=dict=1MiB'
-else
-INITRAMFS_IMAGE=initramfs.cpio.gz
-INITRAMFS_COMPRESS_COMMAND='gzip -c'
 endif
 endif
 